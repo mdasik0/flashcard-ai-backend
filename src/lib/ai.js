08e683421ccc572e3dfccd12bfcc4ad71bbba441
Md asik <asikthe1st@gmail.com>
2025-09-06 13:37:10 +0600

    feat: Implement flashcard generation with question validation

--- src/lib/ai.js
+++ src/lib/ai.js
@@ -1,3 +1,9 @@
-const generateFlashcard = async () => {
+const generateFlashcard = async (question) => {
+    console.log('question from ai.js', question);
+
     
+
+    return {answer: 'this is a dummy answer from ai.js'};
 }
+
+module.exports = generateFlashcard
--- src/routes/aiRoute.js
+++ src/routes/aiRoute.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const generateFlashcard = require('../lib/ai');
 const router = express.Router();
 
 router.get('/generate-flashcard', async (req, res) => {
     try {
         const {question} = req.body;
-        res.send(question)
+
+        if(question === undefined || question.length === "" || typeof question !== 'string'){
+            return res.status(400).send({success: false, message: "Question is required and must be a string"});
+        }
+        const trimmedQuestion = question.trim();
+
+        if(trimmedQuestion.length <= 10){
+            return res.status(400).send({success: false, message: "Question must be at least 10 characters long",queLength : trimmedQuestion.length});
+        }
+        if(trimmedQuestion.length > 100){
+            return res.status(400).send({success: false, message: "Question must be less than 100 characters long",queLength : trimmedQuestion.length});
+        }
+
+        const result = await generateFlashcard(trimmedQuestion);
+        return res.send({result,queLength : trimmedQuestion.length})
     } catch (error) {
         console.log("Error generating flashcard", error);
         res.status(500).send({success: false, message: "Internal Server Error"});
