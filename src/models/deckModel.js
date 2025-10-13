ed403ab89f3e3e3cd55138affd45b7af58be1181
Md asik <asikthe1st@gmail.com>
2025-10-13 07:47:52 +0600

    feat: Add active field to deck model and update createDeck logic for deck existence check

--- src/models/deckModel.js
+++ src/models/deckModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const deckSchema = new mongoose.Schema({
     deckName: {type:String, trim:true, required: true, minlength:3, maxlength:50},
     creatorId: {type:String, required:true},
-    deckImage: {type:String}, privacy: {type:Boolean, required:true}
+    deckImage: {type:String}, privacy: {type:Boolean, required:true}, active: {type:Boolean, required: true}
 },{timestamps: true})
 
 module.exports = mongoose.model("Deck",deckSchema)
